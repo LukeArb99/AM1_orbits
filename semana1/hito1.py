29c80a0058cd1e73efb66665f2b920b45dbe5500
LukeArb99 <jorge4lucas@gmail.com>
2023-09-29 12:55:35 +0200

    actualizacion

--- semana1/hito1.py
+++ semana1/hito1.py
@@ -1,16 +1,5 @@
-
 from numpy  import array, zeros, linalg
-from scipy.optimize import newton
 import matplotlib.pyplot as plt
-
-## FUNCION PARA KEPLER
-# def F_Kepler(U):
-
-#     x,y,vx,vy = U[0], U[1], U[2], U[3]
-
-#     mr = (x**2+y**2)**1.5
-    
-#     return array([vx,vy,-x/mr, -y/mr])
 
 N=1000
 U=array([1,0,0,1])
@@ -24,7 +13,6 @@ y[0]=U[1]
 ## EULER
 for i in range(0,N):
     F = array([U[2], U[3], -U[0]/(U[0]**2+U[1]**2)**1.5, -U[1]/(U[0]**2+U[1]**2)**1.5])
-    # F = F_Kepler(U)
     U = U + dt*F
 
     x[i]=U[0]
@@ -46,15 +34,12 @@ for i in range(0,N):
     sigmaF = array([0,0,0,0])
     F1 = array([U[2], U[3], -U[0]/(U[0]**2+U[1]**2)**1.5, -U[1]/(U[0]**2+U[1]**2)**1.5])
     F2 = array([U[2], U[3], -U[0]/(U[0]**2+U[1]**2)**1.5, -U[1]/(U[0]**2+U[1]**2)**1.5])
-    # F2 = array([0.1,0.1,0.1,0.1])
     sigma = 1
 
     while sigma >= dt*10:
-        # F2 = array([1.,1.,1.,1.])
         F2 = F2 + sigmaF
         U1 = U + 0.5*dt*(F1+F2)
         F3 = array([U1[2], U1[3], -U1[0]/(U1[0]**2+U1[1]**2)**1.5, -U1[1]/(U1[0]**2+U1[1]**2)**1.5])
-        # print(F3)
 
         sigmaF = F2-F3
         sigma = linalg.norm(sigmaF)
@@ -68,18 +53,6 @@ plt.title("CRANK-NICOLSON")
 plt.axis('equal')
 plt.plot(x,y)
 plt.show()
-
-
-## definicion de funcion para crank-nicolson
-# U = array(zeros[4,N])
-# U[:,0]=array([1,0,0,1])
-# for n in range(0,N-1):
-#     a=U[:,N]
-#     U[:,N+1]= newton(G(x),U[:,N])
-
-
-# def G(x):
-#     return x-a-dt/2*(F_Kepler(a)+F_Kepler(x))
 
 
 ## RK 4
