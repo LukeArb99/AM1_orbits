fccb1fda5ff530e7190a401d993fef1767a06782
LukeArb99 <jorge4lucas@gmail.com>
2023-11-14 18:37:48 +0100

    hito 2

--- semana 2/hito2.py
+++ semana 2/hito2.py
@@ -1,4 +1,12 @@
 from Esquemas_temporales import Euler,Euler_Implicito,Crank_Nicolson,RK4
-from funciones import Problema_de_Cauchy,Kepler
-import matplotlib.pyplot as plt
+from funciones import Kepler
+from simulacion import Simulacion
+from numpy import array                     
 
+U0=array([1,0,0,1])
+
+
+Simulacion(Kepler,U0,Euler)
+Simulacion(Kepler,U0,Euler_Implicito)
+Simulacion(Kepler,U0,Crank_Nicolson)
+Simulacion(Kepler,U0,RK4)
--- semana 2/simulacion.py
+++ semana 2/simulacion.py
@@ -1,45 +1,16 @@
-from Esquemas_temporales import Euler
-from funciones import Problema_de_Cauchy,Kepler
-import matplotlib as plt
-from numpy import linspace, array
+from funciones import Problema_de_Cauchy
+import matplotlib.pyplot as plt
+from numpy import linspace
 
-def Simulation(tf, N, U0): 
-   
-    """ 
-    This is a Simulation code to integrate Cauchy problems with some numerical scheme.
-
-    The objective of this Milestone is to create different functions or abstractions 
-    by means of functional programming or function composition. 
-    Namely, the following modules are created to allow this functional composition:
-            1) ODES.Cauhy_problem
-            2) ODES.Temporal_schemes
-            3) Physics.Orbits
-    
-    The idea is to create a Cauchy problem abstraction to integrate different physical 
-    problems with different temporal schemes.
-    
-    Different abstractions : 
-                     1) dU/dt = F(U, t) 
-                     2) Temporal scheme to integrate one step 
-                     3) Cauchy problem to perform different steps 
-                         
-     """
+def Simulacion(F,U0,scheme):
+    d_t = 0.01
+    N=1000
+    t=linspace(0,N*d_t,N+1)
 
 
-    t = linspace(0, tf, N)
-    schemes = [  (Euler, None, None ) ]
+    U = Problema_de_Cauchy(F,t,U0,scheme)
 
-    
-    for (method, order, eps)  in schemes:
-
-       if order != None:  
-          U =  Problema_de_Cauchy( Kepler, t, U0, method, q=order, Tolerance=eps ) 
-       else: 
-          U =  Problema_de_Cauchy( Kepler, t, U0, method ) 
-
-       plt.plot( U[:,0] , U[:,1], "." )
-       plt.show()
-
-if __name__ == "__main__":
-
-  Simulation(100, 100, array( [ 1., 0., 0., 1. ] ) )
+    plt.title(scheme)
+    plt.axis('equal')
+    plt.plot(U[0,:], U[1,:])
+    plt.show()
