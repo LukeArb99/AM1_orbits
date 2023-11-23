08f18fae22ea308f1cf1ff4899b11c44f075c3fb
LukeArb99 <jorge4lucas@gmail.com>
2023-11-23 19:42:24 +0100

    actualizar

--- semana 3/error.py
+++ semana 3/error.py
@@ -1,4 +1,5 @@
-from numpy import array,zeros,linspace
+from numpy import array,zeros,linspace,log10,ones,vstack
+from numpy.linalg import norm,lstsq
 from ODES.Probema_Cauchy import Problema_de_Cauchy
 
 
@@ -8,17 +9,42 @@ def Error_Problema_Couchy(N,dt,F,Scheme,orden,U0):
     t1=linspace(0,N*dt,N+1)
     t2=linspace(0,N*dt/2,N+1)
 
-    U1=zeros((N,Nv))
-    U2=zeros((N,Nv))
     Error = zeros((N,Nv))
 
-    U1[:,0]=U0[:,0]
-    U2[:,0]=U0[:,0]
-
-    U1 = Problema_de_Cauchy(F,t1,U1,Scheme)
-    U2 = Problema_de_Cauchy(F,t2,U2,Scheme)
+    U1 = Problema_de_Cauchy(F,t1,U0,Scheme)
+    U2 = Problema_de_Cauchy(F,t2,U0,Scheme)
 
     for i in range(N):
         Error[i,:]=(U2[2*i,:]-U1[i,:])/(1-1/2**orden)
     
     return Error
+
+
+def Ratio_Convergencia(t,F,U0,Scheme,m):
+    log_E=zeros(m)
+    log_N=zeros(m)
+    N=len(t)-1
+    t1=t
+    U1=Problema_de_Cauchy(F,t1,U0,Scheme)
+
+    for i in range(m):
+        N=2*N
+        t2=array(zeros(N+1))
+        t2[0:N+1:2]=t1, t2[1:N:2]=(t1[1:int(N/2)+1]+t1[0:int(N/2)])/2
+        U2=Problema_de_Cauchy(F,t2,U0,Scheme)
+
+        error=norm(U2[N,:]-U1[int(N/2),:])
+        log_E[i]=log10(error); log_N[i]=log10(N)
+        t1=t2; U1=U2
+
+    for j in range(m):
+        if abs(log_E[j])>12 : break
+
+    j=min(j,m-1)
+    x=log_N[0:j+1]; y=log_E[0:j+1]
+    A=vstack([x,ones(len(x))]).T
+    m,c =lstsq(A,y,rcond=None)[0]
+    order=abs(m)
+    log_E =log_E-log10(1-1/2**order)
+
+    return order, log_E, log_N
--- semana 3/hito3.py
+++ semana 3/hito3.py
@@ -1,11 +1,33 @@
-from error import Error_Problema_Couchy
+from error import Error_Problema_Couchy,Ratio_Convergencia
 from Physics.Orbitas import Kepler
 from ODES.Esquemas_temporales import RK4
-from numpy import array
+from numpy import array, linspace
+import matplotlib.pyplot as plt
 
-N=1000
-dt=0.01
-U0=array([1,0,0,1])
+N = 5000
+dt = 0.01
+t = linspace(0, 10, N) 
+U0 = array( [1, 0, 0, 1 ] )
+order = 1 
 
+print("Error Kepler orbit ") 
+Error, U = Error_Problema_Couchy(N,dt,Kepler,RK4,order,U0)
 
-U=Error_Problema_Couchy(N,dt,Kepler,Scheme=RK4,1,U0)
+plt.plot(t, Error[:,0] )
+plt.axis('equal')
+plt.grid()
+plt.show()
+
+N = 2000
+t = linspace(0, 10, N) 
+U0 = array( [1, 0, 0, 1 ] )
+m = 5
+
+print("Order Euler ") 
+order, log_e, log_n = Ratio_Convergencia( t,Kepler, U0, RK4, m )
+
+print( "order =", order)
+plt.plot(log_n, log_e )
+plt.axis('equal')
+plt.grid()
+plt.show()
